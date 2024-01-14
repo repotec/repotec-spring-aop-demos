d7501ca5a1046a39e9caadbf2074524175f533e3
Ahmed Mamdouh <repotecing@gmail.com>
2024-01-14 23:47:02 +0200

    fix issues

---
+++ args-annotation-pointcut/src/main/java/com/spring/aop/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.spring.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring.aop")
+@EnableAspectJAutoProxy
+public class ApplicationConfig {
+	
+}
